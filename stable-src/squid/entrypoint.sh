5657784aea5de5bc7c34412c6e87b4d26dc4f847
NourEddineX <noureddinex@protonmail.com>
2020-11-26 17:49:13 +0100

    ICAP exclusive mime type

--- stable-src/squid/entrypoint.sh
+++ stable-src/squid/entrypoint.sh
@@ -9,10 +9,18 @@ if [ -f /first-run ] ; then
 	sed -i "s,ICAP_URL,$ICAP_URL,g" /etc/squid/squid.conf
 	sed -i "s,ROOT_DOMAIN,$ROOT_DOMAIN,g" /etc/squid/rewriter
 	rm /first-run
+    if [ ! -z "$ICAP_ALLOW_ONLY_MIME_TYPE" ] ; then
+        for mimetype in $( echo $ICAP_ALLOW_ONLY_MIME_TYPE | tr ',' ' ' ) ; do
+            echo      "acl allowicap rep_mime_type $mimetype" >>  /etc/squid/conf.d/allowicap.conf
+            sed -i    '/allowicap/ s/#//g' /etc/squid/squid.conf
+	    sed -i -E 's/adaptation_access (.*) allow all/adaptation_access \1 deny all/g' /etc/squid/squid.conf
+	    unset ICAP_EXCLUDE_MIME_TYPE 
+	done
+    fi
     if [ ! -z "$ICAP_EXCLUDE_MIME_TYPE" ] ; then
         for mimetype in $( echo $ICAP_EXCLUDE_MIME_TYPE | tr ',' ' ' ) ; do
-            echo "acl noicap rep_mime_type $mimetype" >>  /etc/squid/conf.d/noicap.conf
-            sed -i '/noicap/ s/#//g' /etc/squid/squid.conf
+            echo "acl noicap rep_mime_type $mimetype"    >>  /etc/squid/conf.d/noicap.conf
+            sed -i '/noicap/ s/#//g'    /etc/squid/squid.conf
         done
     fi
 fi
